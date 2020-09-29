4c6d1b52dc0cd489c1b57b051923edcc04f0f140
Claus.Altena <Claus.Altena@caltena.de>
2020-09-30 00:40:01 +0200

    NonoM

--- cDBConnect.cs
+++ cDBConnect.cs
@@ -27,11 +27,11 @@ namespace TSQLtoCAharp
 
      public void  iInterface()
         {
-            string test = File.ReadAllText(@"json.txt").ToString();
+            string test = File.ReadAllText(@"C:\apps\TSQLtoSharp\TSQLtoCAharp\json.txt").ToString();
             Console.WriteLine("Text: {0}" , test);
             InitPost _InitPost = JsonConvert.DeserializeObject<InitPost>(test);
             string a = _InitPost.cClass.ToString();
-            Console.WriteLine("Hello World! from cDBConnect {0] ",a);
+            Console.WriteLine("Hello World! from cDBConnect {0} ",a);
         }
     }
 }
